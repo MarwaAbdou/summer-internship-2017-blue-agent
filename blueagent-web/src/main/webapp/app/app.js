05a34f3903a2f5cc646d4d33a696e901ab7195ae
Mohga Nabil <Mohga@eg.ibm.com>
2017-08-18 21:20:36 +0200

    Fixing Conversation issues

--- blueagent-web/src/main/webapp/app/app.js
+++ blueagent-web/src/main/webapp/app/app.js
@@ -18,56 +18,12 @@
             avatar: 'http://www.freelanceweb16.fr/wp-content/uploads/2015/08/Woman_Avatar.gif',
             userName: 'User'
         };
-
-       vm.messages_2 = MockMessagesService.beginConversation()[0];
-       
-       setTimeout(function() {
-    	   for (var j=0 ; j<vm.messages_2.length ; j+=1){
-    		   vm.messages.push(vm.messages_2[j]);
-    	   }console.log(vm.messages);
-    	 }, 1500); 
-       
-       vm.context = MockMessagesService.beginConversation()[1];
-//       console.log(vm.context);
-         
+       MockMessagesService.sendMessagesToRest(null,null,vm);
+      
        vm.sendMessage = function(message) {
-           
-           console.log('sendMessage');
-           //var messagesTmp = vm.messages_2;
-          
-           vm.messages_2 = MockMessagesService.sendMessagesToRest(message.text, vm.context[0])[0];
-//           console.log(messagesTmp[0]);
-//           for(var i=0 ; i<messagesTmp.length ; i+=1){
-//        	   vm.messages_2.push(messagesTmp[i]);
-//           }           
-           console.log(vm.messages_2);
-           console.log("first length: "+ vm.messages_2.length);
-           
-           setTimeout(function() {
-        	   for (var j=0 ; j<vm.messages_2.length ; j+=1){
-        		   vm.messages.push(vm.messages_2[j]);
-        	   }console.log(vm.messages);
-        	 }, 1500); 
-           
-           
-           
-           vm.context = MockMessagesService.sendMessagesToRest(message.text, vm.context[0])[1];
-//           console.log(message);
-//           console.log(vm.messages);
-//           console.log(message.text);
-         
+           MockMessagesService.sendMessagesToRest(message.text, vm.context,vm);
        }
        
-
-//       vm.wrapperFn = function(){
-//    	       	   console.log("hellooooooooooo");
-//       }
-        
-//       vm.sendMessage = function(message){
-//    	   console.log('sendMessage');
-//    	   vm.messages = MockMessagesService.sendMessages(message.text);
-//       }
-//        
         $scope.results= MockMessagesService.getResults();
         
         $scope.$on('simple-chat-message-posted', function() {
@@ -77,11 +33,9 @@
     }
 
     /* @ngInject */
-    function MockMessagesService($http ) {
-        this.sendMessagesToRest = sendMessagesToRest;
-        this.getResults = getResults;
-        this.beginConversation = beginConversation;
-   
+    function MockMessagesService($http) {
+    	this.getResults = getResults;
+    	this.sendMessagesToRest = sendMessagesToRest;
         function getResults(){
         	var results_array = [];
         	var _tempObject = {
@@ -95,84 +49,44 @@
         	 });	
         	 results_var.error(function(data, status, headers, config) {
      			alert( "failure message: " + JSON.stringify({data: data}));
-         		});
+         	 });
         	 
 //        	 console.log(results_array);
         	 return results_array;
-        	  }
-        
-        function beginConversation(){
-        	console.log("i am in beginConversation");
-        	var tempObject = {
-        			"input":{"text":""}
-        	},
-        	arrayOfContext = [],
-        	arrayOfObjects = [],
-        	wantedContext,
-        	result = $http.post('/blueagent-web/rest/services/conversation', tempObject);
-        	
-        	result.success(function(data, status, headers, config) {  
-        		
-    			    	var anotherTempObject = {
-            				id: 'BA' + Date.now(),
-            				text: data.output.text[0],
-            				userId: 'hilsqdhfods5990K226DHS01NOHoh',
-            				userName: 'Blue Agent',
-            				avatar: 'http://polyligne.be/wp-content/uploads/2014/06/Man_Avatar.gif',
-            				date: Date.now()
-            			}
-//    			    	console.log(data.output.text[0]);
-    			    	arrayOfObjects.push(anotherTempObject);
-    			    	wantedContext = data.context;
-    			    	arrayOfContext.push(wantedContext);
-//    			    	console.log(data.context);
-//    				console.debug(anotherTempObject);
-        	});
-        	result.error(function(data, status, headers, config) {
-    			alert( "failure message: " + JSON.stringify({data: data}));
-    		});
-//        	console.log(arrayOfObjects);
-//        	console.log(arrayOfObjects);
-        	return [arrayOfObjects,arrayOfContext];
         }
         
-        function sendMessagesToRest(temp, context) {
-        	console.log("Tmp: ", temp);
+        function sendMessagesToRest(textInput, context,appController) {
+        	console.log("textInput: ", textInput);
         	var arrayOfObjects = [],
         	arrayOfContext = [],        	
-        	tempObject = {
-        			"input":{"text":temp},
+            msg = {
+        			"input":{"text":textInput},
         			"context": context
-        	}, 
-        	wantedContext,
-//        	tempObject = JSON.stringify({"input":{"text":temp},"context":contextParameter}); 
-        	
-        	result = $http.post('/blueagent-web/rest/services/conversation', tempObject);
+        	},
+        	result = $http.post('/blueagent-web/rest/services/conversation', msg);
         	
         	result.success(function(data, status, headers, config) {  
         		    			
     			for(var i = 0 ; i < data.output.text.length ; i++){
-    				var anotherTempObject = {
-    						id: 'BA' + Date.now(),
-            				text: data.output.text[i],
-            				userId: 'hilsqdhfods5990K226DHS01NOHoh',
-            				userName: 'Blue Agent',
-            				avatar: 'http://polyligne.be/wp-content/uploads/2014/06/Man_Avatar.gif',
-            				date: Date.now()
-            			}
-//    				console.debug(anotherTempObject);
-        			arrayOfObjects.push(anotherTempObject);
+    				if (data.output.text[i] != ""){
+	    				var anotherTempObject = {
+	    						id: 'BA' + Date.now(),
+	            				text: data.output.text[i],
+	            				userId: 'hilsqdhfods5990K226DHS01NOHoh',
+	            				userName: 'Blue Agent',
+	            				avatar: 'http://polyligne.be/wp-content/uploads/2014/06/Man_Avatar.gif',
+	            				date: Date.now()
+	            			}
+	    				console.debug(anotherTempObject);
+	    				appController.messages.push(anotherTempObject);
+    				}
     			}
-    			wantedContext = data.context;
-    			arrayOfContext.push(wantedContext);
-//    			console.log(arrayOfObjects);
+    			appController.context = data.context;
         	});
         	
         	result.error(function(data, status, headers, config) {
     			alert( "failure message: " + JSON.stringify({data: data}));
     		});
-//        	console.log(arrayOfObjects);
-        	return [arrayOfObjects,arrayOfContext];
-        	}
         }
+    }
 })();
